efffdd44b063ad27c9eea205664d6e6b201a8e7f
alegogit <alex@disiniaje.com>
2016-01-14 14:52:21 +0700

    Installed
    Installed, configs generated

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://myhost/admin/');
+define('HTTP_CATALOG', 'http://myhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://myhost/admin/');
+define('HTTPS_CATALOG', 'http://myhost/');
+
+// DIR
+define('DIR_APPLICATION', '/ocdocroot/admin/');
+define('DIR_SYSTEM', '/ocdocroot/system/');
+define('DIR_LANGUAGE', '/ocdocroot/admin/language/');
+define('DIR_TEMPLATE', '/ocdocroot/admin/view/template/');
+define('DIR_CONFIG', '/ocdocroot/system/config/');
+define('DIR_IMAGE', '/ocdocroot/image/');
+define('DIR_CACHE', '/ocdocroot/system/storage/cache/');
+define('DIR_DOWNLOAD', '/ocdocroot/system/storage/download/');
+define('DIR_LOGS', '/ocdocroot/system/storage/logs/');
+define('DIR_MODIFICATION', '/ocdocroot/system/storage/modification/');
+define('DIR_UPLOAD', '/ocdocroot/system/storage/upload/');
+define('DIR_CATALOG', '/ocdocroot/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'dbhost');
+define('DB_USERNAME', 'dbuser');
+define('DB_PASSWORD', 'dbpass');
+define('DB_DATABASE', 'dbname');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'pre_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://myhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://myhost/');
+
+// DIR
+define('DIR_APPLICATION', '/ocdocroot/catalog/');
+define('DIR_SYSTEM', '/ocdocroot/system/');
+define('DIR_LANGUAGE', '/ocdocroot/catalog/language/');
+define('DIR_TEMPLATE', '/ocdocroot/catalog/view/theme/');
+define('DIR_CONFIG', '/ocdocroot/system/config/');
+define('DIR_IMAGE', '/ocdocroot/image/');
+define('DIR_CACHE', '/ocdocroot/system/storage/cache/');
+define('DIR_DOWNLOAD', '/ocdocroot/system/storage/download/');
+define('DIR_LOGS', '/ocdocroot/system/storage/logs/');
+define('DIR_MODIFICATION', '/ocdocroot/system/storage/modification/');
+define('DIR_UPLOAD', '/ocdocroot/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'dbhost');
+define('DB_USERNAME', 'dbuser');
+define('DB_PASSWORD', 'dbpass');
+define('DB_DATABASE', 'dbname');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'pre_');
